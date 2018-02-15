ed5eba2c63ecdba369487a3c132496bf4da58ef3
IronPeach <Iron_Peach@protonmail.com>
2018-02-15 00:10:35 -0600

    Palindromic v 1.0.1
    Add description of Palindromic. and 1186060307891929990 the magical. No bug (Hope so...)

--- 迴文數.py
+++ 迴文數.py
@@ -11,7 +11,7 @@ def Main(Num):
 		t += 1
 		a = str(a)
 		if a == a[::-1]:
-			print("You got a cycle number by reverse adding %d times!" % (t))
+			print("You got a Palindromic number by reverse adding %d times!" % (t))
 			t = 0
 			while True:
 				answer = input("Wanna try again? type y/n:")
@@ -19,7 +19,7 @@ def Main(Num):
 					break
 				print ('I mean that You should type either y or n, it stands for yes and no!')
 			if answer == 'y':
-				a = input()
+				a = input('The number I want to try is:')
 				n1 = int(a)
 				n2 = int(a[::-1])
 				a = n1 + n2
@@ -42,7 +42,19 @@ def Main(Num):
 			
 			
 #Ask number.
-print('Welcome to use SKA useless cycle machine v 1.0.0!')
-print("What number did you want to try?")
+print('Welcome to use SKA useless Palindromic Number machine v 1.0.1!')
+print('By:Iron_Peach')
+print('If you have bug, please report that to Iron_Peach@protonmail.com')
+while True:
+	answer = input('Did you know what is Palindromic number? y/n:')
+	if answer in ('y','n'):
+		break
+	print('I think you should put lower case y or n for the answer...')
+if answer == 'y':
+	print ('Good, now go to the main part! And I\'m gonna ask you:')
+else:
+	print('Palindromic number mean the number that is the same either fromm left to right, and right to left. For example, 1234567654321 is a Palindromic number because both side are the same!3883 can be one, 77 is one too! The way you change a number to Palindromic number is that you add the number with its opposite, for example, 32 would add 23 because it\'s opposite, 32 + 23 = 55! It\'s a Palindromic! If you don\'t get a Palindromic number, try again with your new number Like 67 + 76 = 143 ; 143 + 341 = 484. But there is some number that can\'t be Palindromic(Maybe, who knows?), those are called Lychrel Number, 196 is the most likely the smallest Lychrel (BTW, the world record of Palindromic is 1186060307891929990, it takes add 261 times to make it Palindromic. Maybe you can be the next world record holder! Keep useign our tools! ^_^). Now, go to the main part and I\'m gonna ask you:')
+print("what number did you want to try?")
 Number = input()
 Main(Number)
+#1186060307891929990! What a magical number! It take 261 times to make a Palindromic!
